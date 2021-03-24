c5c240834110913f874532a97892909dfd05b53a
LEAH-PC\Leah <mail@leahuk.com>
2021-03-24 10:03:06 +0000

    Added bixbi_notify

--- client/client.lua
+++ client/client.lua
@@ -29,12 +29,13 @@ Citizen.CreateThread(function()
 				end
 				TriggerEvent('chatMessage', '[EMS]', { 0, 128, 255 }, " your hospital stay time was extended as you were not officially discharged.")
 				TriggerEvent('chatMessage', '[EMS]', { 0, 128, 255 }, ' You have ' .. hosDuration .. ' seconds left in hospital.')
-				ESX.ShowNotification("Your hospital stay time was extended as you were not officially discharged.")
-				ESX.ShowNotification('You have ' .. hosDuration .. ' seconds left in hospital.')
+				
+				TriggerEvent('bixbi_hospital:notify', 'info', "Your hospital stay time was extended as you were not officially discharged.")
+				TriggerEvent('bixbi_hospital:notify', 'info', 'You have ' .. hosDuration .. ' seconds left in hospital.')
 			end
 
 			if hosDuration == 10 or hosDuration == 30 or hosDuration == 60 or hosDuration == 120 or hosDuration == 300 then
-				ESX.ShowNotification('You have ' .. hosDuration .. ' seconds left in hospital.')
+				TriggerEvent('bixbi_hospital:notify', 'info', 'You have ' .. hosDuration .. ' seconds left in hospital.')
 				TriggerEvent('chatMessage', '[EMS]', { 0, 128, 255 }, ' You have ' .. hosDuration .. ' seconds left in hospital.')
 			end
 		elseif location ~= nil then
@@ -57,7 +58,7 @@ AddEventHandler("bixbi_hospital:send", function(duration, inputLocation)
 				ClearPedTasksImmediately(playerPed)
 			end
 			
-			ESX.ShowNotification('You have been sent to ' .. loc.label .. ' for ' .. duration .. ' seconds.')
+			TriggerEvent('bixbi_hospital:notify', 'info', 'You have been sent to ' .. loc.label .. ' for ' .. duration .. ' seconds.')
 			TriggerEvent('chatMessage', '[EMS]', { 0, 128, 255 }, ' You have been sent to ' .. loc.label .. ' for ' .. duration .. ' seconds.')
 		end
 	else
@@ -71,7 +72,7 @@ AddEventHandler("bixbi_hospital:release", function()
 	local playerPed = GetPlayerPed(-1)
 	SetEntityCoords(playerPed, location.outcoords[1], location.outcoords[2], location.outcoords[3])
 
-	ESX.ShowNotification('You have been released from ' .. location.label .. ' .')
+	TriggerEvent('bixbi_hospital:notify', 'info', 'You have been released from ' .. location.label .. ' .')
 	TriggerEvent('chatMessage', '[EMS]', { 0, 128, 255 }, ' You have been released from ' .. location.label .. '.')
 
 	hosDuration = 0
--- config.lua
+++ config.lua
@@ -19,3 +19,17 @@ Config.Locations = {
 		outcoords = {1841.54, 3668.97, 33.68}
 	}
 }
+
+RegisterNetEvent('bixbi_hospital:notify')
+AddEventHandler("bixbi_hospital:notify", function(type, msg)
+
+	if Config.NotifyType == "t-notify" then
+		if type == '' or type == nil then type = 'info' end
+		exports['t-notify']:Alert({style = type, message = msg})
+	elseif Config.NotifyType == "feedm" then
+		if type == '' or type == nil then type = 'primary' end
+		TriggerEvent("FeedM:showNotification", msg, 2500, type)
+	else
+		ESX.ShowNotification(msg)
+	end
+end)
